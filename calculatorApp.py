11694d63f3f6c60c17313ed887762c9ddedd4239
Forrest <forrest.90h@gmail.com>
2021-04-29 16:43:39 -0700

    added unitTest.py and changed calculator filename

--- calculatorApp.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Please input your first number, a: "))
-b = int(input("Please input your second number, b: "))
-
-print(a, " + ", b, " = ", a+b)
-print(a, " - ", b, " = ", a-b)
-print(a, " * ", b, " = ", a*b)
-print(a, " / ", b, " = ", a/b)
