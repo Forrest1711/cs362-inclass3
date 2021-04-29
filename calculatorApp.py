ca78948cb8993daebe60778bdbb543dbc902c1a8
Forrest <forrest.90h@gmail.com>
2021-04-29 16:33:31 -0700

    added calculator app

---
+++ calculatorApp.py
@@ -0,0 +1,7 @@
+a = int(input("Please input your first number, a: "))
+b = int(input("Please input your second number, b: "))
+
+print(a, " + ", b, " = ", a+b)
+print(a, " - ", b, " = ", a-b)
+print(a, " * ", b, " = ", a*b)
+print(a, " / ", b, " = ", a/b)
