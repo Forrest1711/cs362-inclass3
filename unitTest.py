5b37ffc93b75fb0798b5a02a9a09f4b3d1b6f295
Forrest <forrest.90h@gmail.com>
2021-04-29 17:03:30 -0700

    added more tests

--- unitTest.py
+++ unitTest.py
@@ -7,7 +7,31 @@ class testCaseAdd(unittest.TestCase):
         self.assertEqual(calc.add(12, 22), 34)
 
     def test_add_2(self):
-        self.assertEqual(calc.add(3, 7), 21)
+        self.assertEqual(calc.add(3, 7), 10)
+
+
+class testCaseSubtract(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.subtract(12, 22), -10)
+
+    def test_add_2(self):
+        self.assertEqual(calc.subtract(3, 7), -4)
+
+
+class testCaseMultiply(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.multiply(12, 22), 12*22)
+
+    def test_add_2(self):
+        self.assertEqual(calc.multiply(3, 7), 3*7)
+
+
+class testCaseDivide(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.divide(12, 22), 12/22)
+
+    def test_add_2(self):
+        self.assertEqual(calc.divide(3, 7), 3/7)
 
 
 if __name__ == '__main__':
