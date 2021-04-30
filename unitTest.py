a4a7f953f8d7d7bcf6415713c8abbe19523affa4
Forrest <forrest.90h@gmail.com>
2021-04-29 17:40:25 -0700

    finished with tests

--- unitTest.py
+++ unitTest.py
@@ -9,29 +9,41 @@ class testCaseAdd(unittest.TestCase):
     def test_add_2(self):
         self.assertEqual(calc.add(3, 7), 10)
 
+    def test_add_3(self):
+        self.assertEqual(calc.add("banana", 12), "Error")
+
 
 class testCaseSubtract(unittest.TestCase):
-    def test_add(self):
+    def test_subtract(self):
         self.assertEqual(calc.subtract(12, 22), -10)
 
-    def test_add_2(self):
+    def test_subtract_2(self):
         self.assertEqual(calc.subtract(3, 7), -4)
+
+    def test_subtract_3(self):
+        self.assertEqual(calc.subtract("apple", 11), "Error")
 
 
 class testCaseMultiply(unittest.TestCase):
-    def test_add(self):
+    def test_multiply(self):
         self.assertEqual(calc.multiply(12, 22), 12*22)
 
-    def test_add_2(self):
+    def test_multiply_2(self):
         self.assertEqual(calc.multiply(3, 7), 3*7)
+
+    def test_multiply_3(self):
+        self.assertEqual(calc.multiply("orange", 67), "Error")
 
 
 class testCaseDivide(unittest.TestCase):
-    def test_add(self):
+    def test_divide(self):
         self.assertEqual(calc.divide(12, 22), 12/22)
 
-    def test_add_2(self):
+    def test_divide_2(self):
         self.assertEqual(calc.divide(3, 7), 3/7)
+
+    def test_divide_3(self):
+        self.assertEqual(calc.divide("kiwi", 69), "Error")
 
 
 if __name__ == '__main__':
